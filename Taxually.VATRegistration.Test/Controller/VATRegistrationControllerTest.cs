3e4ffb5a93130fdee302fa82ff48fff27e485baa
aarongitmaster <85446159+aarongitmaster@users.noreply.github.com>
2023-01-03 01:38:40 +0100

    Add files via upload

---
+++ Taxually.VATRegistration.Test/Controller/VATRegistrationControllerTest.cs
@@ -0,0 +1,46 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Taxually.TechnicalTest.Controllers;
+using Taxually.TechnicalTest.Service;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Taxually.TechnicalTest.Models;
+
+namespace Taxually.VATRegistration.Test.Controller
+{
+    public class VATRegistrationControllerTest
+    {
+        private Fixture fixture;
+        private VatRegistrationController _vatRegistrationController;
+        private Mock<IRegistrationService> _registrationServiceMock;
+        private VatRegistrationRequest _vatRegistrationRequestModel;
+        private Mock<ILogger> logger;
+
+        [SetUp]
+        public void Setup()
+        {
+            _registrationServiceMock = new Mock<IRegistrationService>();
+            fixture = new Fixture();
+            fixture.Customize(new AutoMoqCustomization());
+              this.logger = this.fixture.Freeze<Mock<ILogger>>();
+            _vatRegistrationRequestModel = fixture.Create<VatRegistrationRequest>();
+            _vatRegistrationController = new VatRegistrationController(_registrationServiceMock.Object);
+        }
+
+        [Test]
+        public void SendRequest()
+        {
+            bool resultReturned;
+            _vatRegistrationController.ControllerContext = new ControllerContext();
+            _vatRegistrationController.ControllerContext.HttpContext = new DefaultHttpContext();
+
+            _registrationServiceMock.Setup(x =>  x.XMLRegistration(_vatRegistrationRequestModel)).Returns(Task.FromResult(_vatRegistrationRequestModel));
+            var result = _vatRegistrationController.PostAsync(_vatRegistrationRequestModel);
+            resultReturned = (bool)((result.Result) as ObjectResult).Value;
+            Assert.False(resultReturned);//failed by precondition
+            Assert.True(result.IsCompletedSuccessfully);
+        }
+    }
+}
---
+++ Taxually.VATRegistration.Test/Services/VATRegistrationServiceTest.cs
@@ -0,0 +1,82 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using Moq;
+using Taxually.TechnicalTest.Service;
+using Taxually.TechnicalTest.Models;
+using Taxually.TechnicalTest.Infrastructure.Mappers;
+
+namespace Taxually.VATRegistration.Test.Controller
+{
+    public class RegistrationServiceTest
+    {
+        private Fixture fixture;
+        private VatRegistrationRequest _vatRegistrationRequestModel;
+        private Mock<IVATRegistrationMapper> _mockmapper;
+
+        [SetUp]
+        public void Setup()
+        {
+            fixture = new Fixture();
+            fixture.Customize(new AutoMoqCustomization());
+            _vatRegistrationRequestModel = fixture.Create<VatRegistrationRequest>();
+            _mockmapper = new Mock<IVATRegistrationMapper>();
+        }
+
+        [Test]
+        public void RegisterinAPI()
+        {
+
+            #region Arrange
+            var VATService = new RegistrationService(_mockmapper.Object);
+            #endregion
+
+            #region Act
+            var returnedObject = VATService.APIRegistration(_vatRegistrationRequestModel);
+            #endregion
+
+            #region Assert
+            Assert.IsInstanceOf(typeof(VatRegistrationRequest), returnedObject.Result);
+            Assert.True((returnedObject.Result as VatRegistrationRequest).IsValid);
+            #endregion
+
+        }
+
+        [Test]
+        public void RegisterXML()
+        {
+
+            #region Arrange
+            var VATService = new RegistrationService(_mockmapper.Object);
+            #endregion
+
+            #region Act
+            var returnedObject = VATService.XMLRegistration(_vatRegistrationRequestModel);
+            #endregion
+
+            #region Assert
+            Assert.IsInstanceOf(typeof(VatRegistrationRequest), returnedObject.Result);
+            Assert.True((returnedObject.Result as VatRegistrationRequest).IsValid);
+            #endregion
+
+        }
+
+        [Test]
+        public void RegisterCVS()
+        {
+
+            #region Arrange
+            var VATService = new RegistrationService(_mockmapper.Object);
+            #endregion
+
+            #region Act
+            var returnedObject = VATService.CSVRegistration(_vatRegistrationRequestModel);
+            #endregion
+
+            #region Assert
+            Assert.IsInstanceOf(typeof(VatRegistrationRequest), returnedObject.Result);
+            Assert.True((returnedObject.Result as VatRegistrationRequest).IsValid);
+            #endregion
+
+        }
+    }
+}
